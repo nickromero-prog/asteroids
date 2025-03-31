f27391d08c98fa2d072da1ef2da1e676fb00c2f6
Nick Romero <romeronick93@gmail.com>
2025-03-31 11:05:47 -0400

    feat(*): add movement functions to player class

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 import pygame
 from constants import *
+from player import Player
+
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         
+        player.update(dt)
         screen.fill("BLACK")
+        player.draw(screen)
         pygame.display.flip()
         # limit the framerate to 60 FPS
         dt = clock.tick(60) / 1000  # convert milliseconds to seconds
